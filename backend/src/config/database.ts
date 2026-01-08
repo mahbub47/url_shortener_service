d861309947f4be6030a9e3eee025ce5bae14b02b
Mahbub Ahmed <94840820+mahbub47@users.noreply.github.com>
2026-01-09 01:33:27 +0600

    chore: backend connected with database

---
+++ backend/src/config/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed");
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
+import connectDB from "./config/database.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
